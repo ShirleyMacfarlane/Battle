50132104de512bd02332e42ade2c9c0f186aabfc
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-02-09 15:57:25 +0000

    Step 18: Getting test infrastructure set up

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes would go here
+  get '/' do
+    'Testing infrastructure working!'
+ end
+
+  # # Start the server if this file is executed directly (do not change the line below)
+  run! if app_file == $0
+end
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,9 @@
+require "capybara/rspec"
+require_relative "../../app"
+
+feature 'Testing the infrastructure' do
+  scenario 'The user visits the homepage' do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
